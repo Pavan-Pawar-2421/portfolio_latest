92ae941c17e01b4e450798795bdb50e3426b2826
Mahesh Bora <boraamahesh@gmail.com>
2025-06-26 21:11:42 +0530

    initial commit

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+import styles from "./Contact.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Contact = () => {
+  return (
+    <footer id="contact" className={styles.container}>
+      <div className={styles.text}>
+        <h2>Contact</h2>
+        <p>Feel free to reach out!</p>
+      </div>
+      <ul className={styles.links}>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
+          <a href="mailto:pawarps_1@rknec.edu@email.com">pawarps_1@rknec.edu</a>
+        </li>
+        <li className={styles.link}>
+          <img
+            src={getImageUrl("contact/linkedinIcon.png")}
+            alt="LinkedIn icon"
+          />
+          <a href="https://www.linkedin.com/in/pavan-pawar24/">linkedin.com/pavan-pawar24</a>
+        </li>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon" />
+          <a href="https://github.com/Pavan-Pawar-2421">github.com/Pavan-Pawar-2421</a>
+        </li>
+      </ul>
+    </footer>
+  );
+};
